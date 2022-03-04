65d4b902ce7a60efdfad2f5e7d8056fabf2126ea
khoirulromadhon <khoirulromadhon1112@gmail.com>
2022-03-04 23:10:32 +0700

    Praktikum 3

--- src/index.js
+++ src/index.js
@@ -1,22 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Test from './Test';
 import reportWebVitals from './reportWebVitals';
+import { render } from '@testing-library/react';
+import HelloComponent from './Component/HelloComponent';
 
+// const HelloComponent = () => {
+//   return <p>StateFullComponent</p>
+// }
 
-const Hello = () =>{
-  return <p>hello</p>
-}
-
-ReactDOM.render(
+// class StateFullComponent extends React.Component{
+//   render(){
+//     return<p>StateFullComponent</p>
+//   }
+// }
+// ReactDOM.render(<HelloComponent/>, document.getElementById('root'));
+ReactDOM.render( 
   <React.StrictMode>
-    <App />
+    <HelloComponent />
   </React.StrictMode>,
   document.getElementById('root')
 );
+// function HelloComponent() {
+//   return HelloComponent
+
+
+
+// const Hello = () =>{
+//   return <p>hello</p>
+// }
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+// }
